68cdc6829b12b0c97c415af98d4451b17bcf4185
seven1oe <seven1oe@users.noreply.github.com>
2019-07-27 19:36:26 +0800

    modify for 360WIFI-P2

---
+++ trunk/configs/boards/360WIFI-P2/board.h
@@ -0,0 +1,30 @@
+/* ASUS RT-AC1200 */
+
+#define BOARD_PID		"360WIFI-P2"
+#define BOARD_NAME		"360WIFI-P2"
+#define BOARD_DESC		"360WIFI P2 Wireless Router"
+#define BOARD_VENDOR_NAME	"FENGLIAN Technology Co., Ltd."
+#define BOARD_VENDOR_URL	"http://luyou.360.cn/index_home.html"
+#define BOARD_MODEL_URL		"http://luyou.360.cn/product_p2.html?from=nav"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#undef 	BOARD_GPIO_BTN_RESET	
+#define BOARD_GPIO_BTN_WPS	38
+#define BOARD_GPIO_BTN_FN1	3
+#undef 	BOARD_GPIO_LED_ALL	//
+#define BOARD_GPIO_LED_WIFI	44
+#define BOARD_GPIO_LED_POWER	11
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#define BOARD_GPIO_LED_USB	46
+#define BOARD_GPIO_LED_ROUTER	37
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
+
